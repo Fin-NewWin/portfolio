df1ce1b28442c3fc385323a5225ad7fe6fe1b97f
Fin-NewWin <nguyenphien17@yahoo.com>
2022-04-20 03:51:23 -0700

    updated package version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import React from 'react';
+import { Dimensions } from 'react-native';
+import pfp from './lorax_pfp.jpg';
 
 function App() {
   return (
-    <div>
-        <div class="Main">
-            This is something 
+    <div class="Main">
+        <div class="top">
+            <div float="left"> 
+                <img width={Dimensions.get('window').width/6}  src={pfp} alt="pfp" />
+            </div>
+            <div float="right">
+                <h1>Bio</h1>
+                <p>I'm currently a junior at University of California majoring in Computer Science and Engineering</p>
+            </div>
         </div>
     </div>
   );
