b1548e1b0b49e43538970382e963bc06dbf329a5
Phien Nguyen <nguyenphien17@yahoo.com>
2023-10-09 11:42:55 -0700

    chore: format prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import React from 'react';
-import pfp from './lorax_pfp.jpg';
+import "./App.css";
+import React from "react";
+import pfp from "./lorax_pfp.jpg";
 
 function App() {
     return (
-        <div className='intro'>
-            <div className='intro-left'>
-                <div className='intro-left-wrapper'>
+        <div className="intro">
+            <div className="intro-left">
+                <div className="intro-left-wrapper">
                     <h2>Hello</h2>
                 </div>
             </div>
-            <div className='intro-right'>
+            <div className="intro-right">
                 <h2>TODO: pfp</h2>
             </div>
         </div>
